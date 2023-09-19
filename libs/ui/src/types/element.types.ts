e6f78e9de3536cae330266925c3a275cba75c805
jackmcosta32 <joaomcosta32@gmail.com>
2023-09-19 14:39:13 -0300

    refactor(ui): removes unused type folder

--- libs/ui/src/types/element.types.ts
+++
@@ -1,4 +0,0 @@
-export type TElementProps<TIntrinsicElements = 'div'> =
-  TIntrinsicElements extends keyof JSX.IntrinsicElements
-    ? JSX.IntrinsicElements[TIntrinsicElements]
-    : Record<never, never>;
