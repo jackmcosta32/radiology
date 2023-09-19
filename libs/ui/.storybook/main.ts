2f0010fdebe9484146267103075c00d2f39c6e27
jackmcosta32 <joaomcosta32@gmail.com>
2023-09-19 17:24:33 -0300

    chore(ui): ensures storybook usage os postcss correct version

--- libs/ui/.storybook/main.ts
+++ libs/ui/.storybook/main.ts
@@ -2,7 +2,19 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+          loadSassAfterPostCSS: true,
+        },
+      },
+    },
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
