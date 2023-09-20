8f4db5b0355487766431d0a19adc2ea402990297
jackmcosta32 <joaomcosta32@gmail.com>
2023-09-20 10:00:54 -0300

    refactor(ui): refactors the header component

--- libs/ui/src/components/header/header.tsx
+++ libs/ui/src/components/header/header.tsx
@@ -5,16 +5,12 @@ export function Header({ children, className, ...rest }: HeaderProps) {
   return (
     <header
       className={twMerge(
-        ['px-6 py-3 flex items-center justify-between border-b min-w-full'],
+        ['px-6 py-4 flex items-center justify-between border-b min-w-full'],
         className
       )}
       {...rest}
     >
-      <div className="w-7xl w-full mx-auto">
-        <h1 className="text-xl font-bold">Radiology</h1>
-
-        <div className="flex gap-3 items-center">{children}</div>
-      </div>
+      {children}
     </header>
   );
 }
