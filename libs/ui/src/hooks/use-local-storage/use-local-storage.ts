4ffc14fcc857296ed52b2e4f44ca564bc9f375a2
jackmcosta32 <joaomcosta32@gmail.com>
2023-09-27 18:37:17 -0300

    feat(ui): adds useLocalStorage hook for storing state in localStorage

---
+++ libs/ui/src/hooks/use-local-storage/use-local-storage.ts
@@ -0,0 +1,35 @@
+import { Dispatch, useState } from 'react';
+import { isServerSide } from '../../utils/server-side/is-server-side';
+
+export function useLocalStorage<Value>(key: string, initialValue?: Value) {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (isServerSide(document)) {
+      return initialValue;
+    }
+
+    const item = window.localStorage.getItem(key);
+
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = (value: Value) => {
+    let valueToStore = value;
+
+    if (typeof value === 'function') {
+      valueToStore = value();
+    }
+
+    setStoredValue(valueToStore);
+
+    if (isServerSide(document)) return;
+
+    const stringifyedValue = JSON.stringify(valueToStore);
+
+    window.localStorage.setItem(key, stringifyedValue);
+  };
+
+  return [
+    storedValue as Value | undefined,
+    setValue as Dispatch<Value>,
+  ] as const;
+}
